9a41918d3bd9b005eb497742dd0fc02d2edfb6e8
SayedAli <sayed.ali.majed@gmail.com>
2025-07-21 16:20:57 +0300

    Excerise 3 compeleted

--- app.js
+++ app.js
@@ -27,4 +27,33 @@ function isAdult(age) {
    return result;
   }
 console.log('Exercise 2 Result:', isAdult(21));
+console.log('Exercise 2 Result:', isAdult(17));
+/*
+Exercise 3: isCharAVowel()
 
+Write a function named isCharAVowel that takes a single character as 
+an argument. It should return true if the character is a vowel and 
+false otherwise. For the purposes of this exercise, the character 
+y should not be considered a vowel.
+
+Example: isCharAVowel('a') should return true.
+
+Complete the exercise in the space below:*/
+
+function isCharAVowel(char) {
+  if (char ==='a') {
+    return true;
+  } else if (char === 'e'){
+    return true;
+  } else if (char === 'i') {
+    return true;
+  } else if (char === 'o') {
+    return true;
+  } else if (char === 'u') {
+    return true;
+  } else {
+    return false;
+  }
+}
+console.log('Exercise 3 Result:', isCharAVowel("a"));
+console.log('Exercise 3 Result:', isCharAVowel("b"));
